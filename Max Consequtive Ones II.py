25852d4b2695188975fac42dfcea90ac0e8e799a
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-06-26 20:27:28 -0700

    Update Max Consequtive Ones II.py

--- Max Consequtive Ones II.py
+++ Max Consequtive Ones II.py
@@ -11,6 +11,6 @@ class Solution:
                     l+=1
                 flip-=1
             max_len = max(max_len,(r-l+1))
-            right+=1
+            r+=1
             
         return max_len
