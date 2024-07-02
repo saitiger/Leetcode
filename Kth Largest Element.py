262cd5ff4cf067ef0dfb3e6ce568596019eae0a2
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-07-02 11:51:46 -0700

    Update Kth Largest Element.py

--- Kth Largest Element.py
+++ Kth Largest Element.py
@@ -1,4 +1,13 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
+       # Solution 1
+        
         nums.sort()
         return nums[len(nums) - k]
+
+        # Solution 2 
+        heapq._heapify_max(nums)
+        while k>1:
+            heapq._heappop_max(nums)
+            k-=1
+        return nums[0]
