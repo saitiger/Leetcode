1b7728113246adb60d97b1624c5bfec5e5ee2370
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-07-10 18:21:15 -0700

    Update Missing Number.py

--- Missing Number.py
+++ Missing Number.py
@@ -14,3 +14,17 @@ class Solution:
         # for n in nums:
             # res^ = n
         # return res
+
+    # Cyclic Sort 
+        i = 0
+        while i<len(nums):
+            correct_idx = nums[i]
+            if(correct_idx<len(nums) and nums[i]!=i):
+                nums[i],nums[correct_idx] = nums[correct_idx],nums[i]
+            else:
+                i+=1
+        
+        for j in range(len(nums)):
+            if(j!=nums[j]):
+                return j
+        return len(nums)
