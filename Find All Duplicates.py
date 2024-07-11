20116cf9c66ac9725d8fbbf202a940865a3ef344
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-07-10 18:58:05 -0700

    Create Find All Duplicates.py

---
+++ Find All Duplicates.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        i = 0
+        res = []
+        while i<len(nums):
+            correct_idx = nums[i] - 1 
+            if(correct_idx<len(nums) and nums[i]!=nums[correct_idx]):
+                nums[i],nums[correct_idx] = nums[correct_idx],nums[i]
+            else:
+                i+=1
+        for j in range(len(nums)):
+            if(nums[j]!=j+1):
+                res.append(nums[j])
+        return res
