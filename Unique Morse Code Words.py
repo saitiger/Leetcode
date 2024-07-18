ded65021f5dfd96ffff736a3f188cd16a4d6931b
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-07-18 13:13:08 -0700

    Create Unique Morse Code Words.py

---
+++ Unique Morse Code Words.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        MORSE = [".-","-...","-.-.","-..",".","..-.","--.",
+                 "....","..",".---","-.-",".-..","--","-.",
+                 "---",".--.","--.-",".-.","...","-","..-",
+                 "...-",".--","-..-","-.--","--.."]
+
+        ans = {"".join(MORSE[ord(c) - ord('a')] for c in word)
+                for word in words}
+        return len(ans)
