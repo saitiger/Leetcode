11e4f6810677690db3965bf73d50dac82108d3bd
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-07-18 18:10:38 -0700

    Update Longest Substring without Repeating Character.py

--- Longest Substring without Repeating Character.py
+++ Longest Substring without Repeating Character.py
@@ -1,14 +1,28 @@
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         n = len(s)
+#         maxLength = 0
+#         charMap = {}
+#         left = 0
+#         for right in range(n):
+#             if s[right] not in charMap or charMap[s[right]] < left:
+#                 charMap[s[right]] = right
+#                 maxLength = max(maxLength, right - left + 1)
+#             else:
+#                 left = charMap[s[right]] + 1
+#                 charMap[s[right]] = right
+#         return maxLength
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        n = len(s)
-        maxLength = 0
-        charMap = {}
-        left = 0
-        for right in range(n):
-            if s[right] not in charMap or charMap[s[right]] < left:
-                charMap[s[right]] = right
-                maxLength = max(maxLength, right - left + 1)
-            else:
-                left = charMap[s[right]] + 1
-                charMap[s[right]] = right
-        return maxLength
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
