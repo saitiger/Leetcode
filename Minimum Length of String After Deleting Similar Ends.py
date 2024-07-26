4fb60a284c6ae447b6b23752b1e6682c1a96a300
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-07-26 12:29:05 -0700

    Update Minimum Length of String After Deleting Similar Ends.py

--- Minimum Length of String After Deleting Similar Ends.py
+++ Minimum Length of String After Deleting Similar Ends.py
@@ -1,6 +1,8 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
         l, r = 0, len(s) - 1
+        if(s[l]!=s[r]):
+            return len(s)
         while l < r and s[l] == s[r]:
             tmp = s[l]
             while l <= r and s[l] == tmp:
