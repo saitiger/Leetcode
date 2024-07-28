2ff07e962cabba3d024d6db616ba9f811c1b876c
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-07-28 13:11:49 -0700

    Update Power of Four.py

--- Power of Four.py
+++ Power of Four.py
@@ -4,6 +4,10 @@ class Solution:
         if n<=0:
             return False
         return log(n,4)%1==0
+        
+        # Solution 2 Using Bit Mask
+        return n > 0 and (n & (n - 1)) == 0 and (n - 1) % 3 == 0
+
 
         # if n==1:
         #     return True
