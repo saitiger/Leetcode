d32b82608f7fac0c89cd0ff9f7e24cf69804a11b
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-07-29 15:26:22 -0700

    Update Word Pattern.py

--- Word Pattern.py
+++ Word Pattern.py
@@ -14,3 +14,17 @@ class Solution:
             elif mpp.get(words[i]) != pattern[i]:
                 return False
         return True
+        
+        # Solution 2 
+        from collections import defaultdict
+        charToIndex = defaultdict(int)
+        wordToIndex = defaultdict(int)
+        words = s.split()
+        if len(words) != len(pattern):
+            return False
+        for i in range(len(pattern)):
+            char, word = pattern[i], words[i]
+            if charToIndex[char] != wordToIndex[word]:
+                return False
+            charToIndex[char] = wordToIndex[word] = i + 1
+        return True
