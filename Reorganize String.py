f8e64b2b813dbd705931619a1ef10b0d6f1c06d3
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-07-30 16:00:09 -0700

    Update Reorganize String.py

--- Reorganize String.py
+++ Reorganize String.py
@@ -19,3 +19,25 @@ class Solution:
             if cnt != 0:
                 prev = [cnt, char]
         return res
+# Method 2 
+        from collections import Counter
+        n = len(s)
+        count = Counter(s)
+        maxFreq = max(count.values())
+        maxFreqCh = max(count, key=count.get)
+        if maxFreq > (n + 1) // 2:
+            return ""
+        result = [''] * n
+        index = 0
+        while count[maxFreqCh] > 0:
+            result[index] = maxFreqCh
+            index += 2
+            count[maxFreqCh] -= 1
+        for ch, freq in count.items():
+            while freq > 0:
+                if index >= n:
+                    index = 1
+                result[index] = ch
+                index += 2
+                freq -= 1
+        return ''.join(result)
