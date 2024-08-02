b356f49d72d03a7e7ee43d0c608e4d5b17cceb56
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-08-02 15:59:16 -0700

    Create Minimum Swap to Group All 1's Together.py

---
+++ Minimum Swap to Group All 1's Together.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        cnt = 0
+        m = len(nums)
+        for n in nums:
+            if n==1:
+                cnt+=1
+        
+        i,j,res,cnt_zero = 0,0,float(inf),0
+
+        while j<m:
+            if nums[j]==0:
+                cnt_zero+=1
+            if(j-i+1==cnt):
+                res = min(res,cnt_zero)
+                if (nums[i]==0):
+                    cnt_zero-=1
+                i+=1
+            j+=1
+        return res
