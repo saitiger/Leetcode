b28eb89713ac461c7e52043f3845f91959c363da
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-08-06 12:34:06 -0700

    Update K Closest Points to Origin.py

--- K Closest Points to Origin.py
+++ K Closest Points to Origin.py
@@ -18,3 +18,17 @@ class Solution:
             dist, x, y = heapq.heappop(minHeap)
             res.append((x, y))
         return res
+# Solution 3 
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        max_heap = []
+        for x, y in points:
+            dist = x**2 + y**2
+
+            if len(max_heap) < k:
+                heapq.heappush(max_heap, (-dist, x, y))
+            else:
+                heapq.heappush(max_heap, (-dist, x, y))
+                heapq.heappop(max_heap)
+                
+        return [(x, y) for d, x, y in max_heap]
