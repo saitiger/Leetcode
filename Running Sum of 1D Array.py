2420f0c8a215692d551d5b8c6cda4cc3f501cfcb
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-08-07 15:57:27 -0700

    Create Running Sum of 1D Array.py

---
+++ Running Sum of 1D Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        res = []
+        s = 0
+        for n in nums:
+            s+=n
+            res.append(s)
+        return res
