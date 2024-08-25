b5bab37a5b51f8d3ee13d1b118174e28b802e060
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-08-25 00:45:45 -0700

    Update Buy Two Chocolates.py

--- Buy Two Chocolates.py
+++ Buy Two Chocolates.py
@@ -9,3 +9,17 @@ class Solution:
             num_chocolates-=1
             i+=1
         return res if res>=0 and num_chocolates==0 else money
+
+# Optimal (Leetcode Editorial)
+        minimum = min(prices[0], prices[1])
+        second_minimum = max(prices[0], prices[1])
+        for i in range(2, len(prices)):
+            if prices[i] < minimum:
+                second_minimum = minimum
+                minimum = prices[i]
+            elif prices[i] < second_minimum:
+                second_minimum = prices[i]
+        min_cost = minimum + second_minimum
+        if min_cost <= money:
+            return money - min_cost
+        return money
