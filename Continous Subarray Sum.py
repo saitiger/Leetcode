c2358258b49c4c73acf9b66680dd07655cd8d156
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-09-03 16:50:50 -0700

    Update Continous Subarray Sum.py

--- Continous Subarray Sum.py
+++ Continous Subarray Sum.py
@@ -1,8 +1,11 @@
+# The intuition is that we find the same mod again if and only if we have encountered elements 
+# that sum up to a multiple of k
+
 class Solution:
     def checkSubarraySum(self, nums, k):
         n = len(nums)
         
-        remainder_map = {0: -1}
+        remainder_map = {0: -1} # Initialze to account for an empty array because 0 is a multiple of k 
         total_sum = 0
         
         for i in range(n):
@@ -10,7 +13,8 @@ class Solution:
             remainder = total_sum % k
             
             if remainder in remainder_map:
-                if i - remainder_map[remainder] >= 2:
+                if i - remainder_map[remainder] >= 2: # Validating if the size of the contiguous array has a size greater 
+                                                      # than or equal to 2 
                     return True
             else:
                 remainder_map[remainder] = i
