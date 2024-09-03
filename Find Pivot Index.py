8ed7c69823ef73bdf9f442a42d14574cf960c022
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-09-03 16:53:20 -0700

    Update Find Pivot Index.py

--- Find Pivot Index.py
+++ Find Pivot Index.py
@@ -1,12 +1,12 @@
+# Iteratively calculate the prefix/lsum. Check if lsum is equal to rsum if not add the current element to lsum and continue 
+# the loop
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
         l_sum = 0
         r_sum = sum(nums)
         for i in range(len(nums)):
-            r_sum -= nums[i]
+            r_sum -= nums[i] # Total - current element 
             if l_sum == r_sum:
                 return i
             l_sum += nums[i]
         return -1
-        
-            
