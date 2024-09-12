aa579720bb477b58fd7d0cfc9329dde2598fb9f3
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-09-11 17:39:19 -0700

    Update Binary Subarray with Sum K.py

--- Binary Subarray with Sum K.py
+++ Binary Subarray with Sum K.py
@@ -44,3 +44,22 @@ class Solution:
 
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
         return self.helper_func(nums, goal) - self.helper_func(nums, goal - 1)
+
+# Solution 3 Using Prefix Sum & Hashmap
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        total_count = 0
+        current_sum = 0
+        freq = {} 
+
+        for num in nums:
+            current_sum += num
+            if current_sum == goal:
+                total_count += 1
+
+            if current_sum - goal in freq:
+                total_count += freq[current_sum - goal]
+
+            freq[current_sum] = freq.get(current_sum, 0) + 1
+
+        return total_count
