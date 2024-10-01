3372b080848e854eb8d568c5f8f7f77bcd107c2d
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-10-01 10:46:55 -0700

    Update Product of Array Except Self.py

--- Product of Array Except Self.py
+++ Product of Array Except Self.py
@@ -1,11 +1,10 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = [1] * (len(nums))
-
         for i in range(1, len(nums)):
-            res[i] = res[i-1] * nums[i-1]
+            res[i] = res[i-1] * nums[i-1] # Prefix i.e product of all elements to the left of the current element 
         postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
+        for i in range(len(nums) - 1, -1, -1): # Traverse right to left to find postfix of the current element 
+            res[i] *= postfix # Product of all the elements to the right 
+            postfix *= nums[i] # Store the postfix for the next iteration 
         return res
