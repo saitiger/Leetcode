893be557929c871969fb6bb00e6110200057b03d
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-11-15 14:59:39 -0800

    Update Count Number of Pairs With Absolute Difference K.py

--- Count Number of Pairs With Absolute Difference K.py
+++ Count Number of Pairs With Absolute Difference K.py
@@ -6,3 +6,14 @@ class Solution:
             cnt+= mpp[num-k] + mpp[num+k]
             mpp[num] += 1
         return cnt
+
+# O(N^2)
+from collections import Counter
+    mpp = Counter(nums)
+    cnt = 0
+    for num in mpp:
+        # Check if `num + k` exists in the dictionary
+        if num + k in mpp:
+            count += mpp[num] * mpp[num + k] # No need to explicitly check `num - k` because it's symmetric i.e we need only one of the pairs i,j and j,i
+
+    return count
