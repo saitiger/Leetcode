00b4450af4e3a423f1ae54be8a3171f663601e03
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-11-15 19:16:56 -0800

    Create Find the Power of K-Size Subarrays I.py

---
+++ Find the Power of K-Size Subarrays I.py
@@ -0,0 +1,19 @@
+class Solution:
+    def resultsArray(self, nums, k):
+        if k == 1:
+            return nums  
+
+        length = len(nums)
+        result = [-1] * (length - k + 1)
+        consecutive_count = 1  
+
+        for index in range(length - 1):
+            if nums[index] + 1 == nums[index + 1]:
+                consecutive_count += 1
+            else:
+                consecutive_count = 1  
+                
+            if consecutive_count >= k:
+                result[index - k + 2] = nums[index + 1]
+
+        return result
