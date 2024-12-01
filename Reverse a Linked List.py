160f4509ede6ff5218f85a2a953f57996ffb6a28
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-11-30 20:46:38 -0800

    Update Reverse a Linked List.py

--- Reverse a Linked List.py
+++ Reverse a Linked List.py
@@ -3,6 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Using Stack  
+def reverse_linked_list(head):
+    temp = head  
+    stack = []   
+    while temp is not None:
+        stack.append(temp.data) 
+        temp = temp.next        
+    temp = head  
+    while temp is not None:
+        temp.data = stack.pop()  
+        temp = temp.next        
+    return head  
+
+# Without Stack 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         current = head
