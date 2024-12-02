70869f8059a83b17e40d7a8ccaf4ff53b3a87069
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-12-02 00:20:53 -0800

    Create Palindrome Linked List.py

---
+++ Palindrome Linked List.py
@@ -0,0 +1,45 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        stack = []
+        temp = head 
+        while temp:
+            stack.append(temp.val)
+            temp = temp.next 
+        temp = head
+        while temp:
+            if temp.val == stack.pop(): 
+                temp = temp.next
+            else:
+                return False 
+        return True
+
+  # Solution 2 : O(1) Space
+    def reverse(self, head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while curr:
+            next_temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_temp
+        return prev
+
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        rev = self.reverse(slow)
+        while rev:
+            if head.val != rev.val:
+                return False
+            head = head.next
+            rev = rev.next
+        return True
