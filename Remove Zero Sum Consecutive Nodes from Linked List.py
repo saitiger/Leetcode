3d5330065879cd204466ac33edce760a6ab5a7c7
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-12-06 20:52:09 -0800

    Create Remove Zero Sum Consecutive Nodes from Linked List.py

---
+++ Remove Zero Sum Consecutive Nodes from Linked List.py
@@ -0,0 +1,39 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def removeZeroSumSublists(self, head: ListNode) -> ListNode:
+        prefix_sum = 0
+        mp = {}
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        mp[0] = dummy
+        
+        current = head
+        
+        while current:
+            prefix_sum += current.val
+            
+            if prefix_sum in mp:
+                # Find the previous node where the same prefix sum occurred
+                P = mp[prefix_sum]
+                start = P
+                p_sum = prefix_sum
+                
+                # Remove all nodes between P and current
+                while start != current:
+                    start = start.next
+                    p_sum += start.val
+                    if start != current:
+                        mp.pop(p_sum, None)
+                
+                P.next = start.next
+            else:
+                mp[prefix_sum] = current
+            
+            current = current.next
+        
+        return dummy.next
