ff40c8b72914bebed4c49c6b13b3064935a6f771
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2024-12-07 11:24:57 -0800

    Update Happy Number.py

--- Happy Number.py
+++ Happy Number.py
@@ -12,3 +12,23 @@ class Solution:
             visited.add(sum_of_squares)            
             n = sum_of_squares
         return True
+        
+# Solution 2 : Using Linked List Cycle Detection
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def squared(n):
+            result = 0
+            while n>0:
+                last = n%10
+                result += last * last
+                n = n//10
+            return result
+        
+        slow = squared(n)
+        fast = squared(squared(n))
+
+        while slow!=fast and fast!=1:
+            slow = squared(slow)
+            fast = squared(squared(fast))
+
+        return fast==1
