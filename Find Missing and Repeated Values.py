cec6b0723608e23c2aa0f174fe8a689b24aebb30
Sai Tiger Raina <54144981+saitiger@users.noreply.github.com>
2025-03-05 18:46:06 -0800

    Update Find Missing and Repeated Values.py

--- Find Missing and Repeated Values.py
+++ Find Missing and Repeated Values.py
@@ -13,3 +13,18 @@ class Solution:
             elif m[i] == 0:
                 b = i
         return [a, b]
+
+# Solution 2 : Math 
+        n = len(grid)
+        total = n * n
+
+        sum_val = sum(num for row in grid for num in row)
+        sqr_sum = sum(num * num for row in grid for num in row)
+
+        sum_diff = sum_val - total * (total + 1) // 2
+        sqr_diff = sqr_sum - total * (total + 1) * (2 * total + 1) // 6
+
+        repeat = (sqr_diff // sum_diff + sum_diff) // 2
+        missing = (sqr_diff // sum_diff - sum_diff) // 2
+
+        return [repeat, missing]
